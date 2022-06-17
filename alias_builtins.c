3b89567cfb9cddbff571716bfc1648abca5c3532
Olumide, S.A <simiolumide@gmail.com>
2022-06-17 09:08:11 -0700

    resolved betty issue

--- alias_builtins.c
+++ alias_builtins.c
@@ -9,22 +9,19 @@ void print_alias(alias_t *alias);
 
 
 /**
+ * shellby_alias - Builtin command that either prints all aliases, specific
  *
- *  * shellby_alias - Builtin command that either prints all aliases, specific
+ * aliases, or sets an alias.
  *
- *   * aliases, or sets an alias.
+ * @args: An array of arguments.
  *
- *    * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
  *
- *     * @front: A double pointer to the beginning of args.
+ * Return: If an error occurs - -1.
  *
- *      *
+ * Otherwise - 0.
  *
- *       * Return: If an error occurs - -1.
- *
- *        *         Otherwise - 0.
- *
- *         */
+ */
 
 int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 
@@ -102,19 +99,16 @@ int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 
 }
 
-
-
 /**
+ * set_alias - Will either set an existing alias 'name' with a new value,
  *
- *  * set_alias - Will either set an existing alias 'name' with a new value,
+ * 'value' or creates a new alias with 'name' and 'value'.
  *
- *   * 'value' or creates a new alias with 'name' and 'value'.
+ * @var_name: Name of the alias.
  *
- *    * @var_name: Name of the alias.
+ * @value: Value of the alias. First character is a '='.
  *
- *     * @value: Value of the alias. First character is a '='.
- *
- *      */
+ */
 
 void set_alias(char *var_name, char *value)
 
@@ -179,14 +173,12 @@ void set_alias(char *var_name, char *value)
 }
 
 
-
 /**
+ * print_alias - Prints the alias in the format name='value'.
  *
- *  * print_alias - Prints the alias in the format name='value'.
+ * @alias: Pointer to an alias.
  *
- *   * @alias: Pointer to an alias.
- *
- *    */
+ */
 
 void print_alias(alias_t *alias)
 
@@ -221,18 +213,15 @@ void print_alias(alias_t *alias)
 }
 
 /**
+ * replace_aliases - Goes through the arguments and replace any matching alias
  *
- *  * replace_aliases - Goes through the arguments and replace any matching alias
+ * with their value.
  *
- *   * with their value.
+ * @args: 2D pointer to the arguments.
  *
- *    * @args: 2D pointer to the arguments.
+ * Return: 2D pointer to the arguments.
  *
- *     *
- *
- *      * Return: 2D pointer to the arguments.
- *
- *       */
+ */
 
 char **replace_aliases(char **args)
 
